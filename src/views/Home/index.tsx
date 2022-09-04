63a8ce7ba1acb1973ba90fdf67c201a920eea3fb
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-09-04 01:48:38 +0100

    Update index.tsx

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -56,7 +56,7 @@ const Home: React.FC = () => {
         background={
           theme.isDark
             ? 'radial-gradient(103.12% 50% at 50% 50%, #21193A 0%, #191326 100%)'
-            : 'linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)'
+            : 'radial-gradient(103.12% 50% at 50% 50%, #21193A 0%, #191326 100%)'
         }
         index={2}
         hasCurvedDivider={false}
