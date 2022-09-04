f6badb67f96609f062424e5150c6790316774631
magikfinance <97064767+magikfinance@users.noreply.github.com>
2022-09-04 18:46:46 +0100

    update

--- src/components/TransactionConfirmationModal/index.tsx
+++ src/components/TransactionConfirmationModal/index.tsx
@@ -38,6 +38,7 @@ function ConfirmationPendingContent({ pendingText }: { pendingText: string }) {
   return (
     <Wrapper>
       <ConfirmedIcon>
+        <Spinner />
       </ConfirmedIcon>
       <AutoColumn gap="12px" justify="center">
         <Text fontSize="20px">{t('Waiting For Confirmation')}</Text>
