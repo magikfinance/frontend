a7653ea2c3e60fc503cc3704888fcec3fc3e9acd
magikfinance <adasibeats@gmail.com>
2022-12-02 17:08:51 +0000

    update gas

--- src/state/types.ts
+++ src/state/types.ts
@@ -54,9 +54,9 @@ export const EMPTY_LIST: TokenAddressMap = {
 }
 
 export enum GAS_PRICE {
-  default = '5',
-  fast = '6',
-  instant = '7',
+  default = '40',
+  fast = '100',
+  instant = '200',
   testnet = '10',
 }
 
